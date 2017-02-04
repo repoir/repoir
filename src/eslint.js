3fd3301ce37a57808819207dff47a87d10980d25
Sean Wesenberg <wookets@gmail.com>
2017-02-04 07:46:01 -0600

    eslint init

---
+++ src/eslint.js
@@ -0,0 +1,8 @@
+
+export default (options) => {
+
+    console.log('Checking eslint');
+
+    
+
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+
+module.exports = function (program) {
+    console.log('REPOIR IN EFFECT2');
+}
