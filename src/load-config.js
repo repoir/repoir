eeb6bbdd89a753d1dc95196f7399909b0724f2db
Cory Roloff <cory@brothersroloff.com>
2017-04-07 15:05:00 -0500

    Resolve program.config when requiring

--- src/load-config.js
+++ src/load-config.js
@@ -1,8 +1,9 @@
 import { merge } from 'lodash';
+import { resolve } from 'path';
 
 export default function loadConfig (program) {
 	const defaultConfig = require(`${__dirname}/../.repoir.json`);
-	const projectConfig = require(program.config);
+	const projectConfig = require(resolve(program.config));
 
 	return merge({}, defaultConfig, projectConfig);
 }
